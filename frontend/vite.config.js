db27bbde81c67d69f2ec0fe6df7cf739018615e4
Bhavesh <info.banglesaffairs@gmail.com>
2026-09-05 19:59:44 +0530

    solve bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
+const currentDir = path.dirname(fileURLToPath(import.meta.url))
+const rootEnvPath = path.resolve(currentDir, '../.env')
+const envDir = fs.existsSync(rootEnvPath) ? path.resolve(currentDir, '..') : currentDir
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir,
 })
+
+
